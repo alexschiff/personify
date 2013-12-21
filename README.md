e7837a29e4c8f1a25c198ead67ecedd8a0ca4062
alexschiff <alex.schiff1128@gmail.com>
2013-12-20 19:05:38 -0500

    Recommit this after Github failure

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-personify
+Personify
 =========
 
-Guess anyone's email address
+Personify is a simple website to help you find people's email addresses. Guess an email address and hit submit, and if it's correct information about that person will pop up on the right.
+
+It's basically just taking the "Rapportive hack" and making it into its own website.
+
+Tech used:
+
+FullContact jQuery plugin to verify email addresses: https://github.com/mattdelliott/fullcontact-api-jquery-plugin
---
+++ scripts.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {
+	
+	$('#submitButtonEmail').click(function() {
+		var email = $('#Email').val();
+	
+
+	$.fullcontact.emailLookup('fdd4a159a9943b57', email, function(obj) {
+		console.log(obj);
+		
+		if (obj.status == 200) {
+
+
+
+		var person = obj
+
+		console.log("Identity found!")
+		console.log(person)
+
+		$('#fullNameHeader').text('Found '+person.contactInfo.fullName+' with '+person.likelihood+' likelihood!')
+		console.log(person.contactInfo.fullName)
+
+		var srcOfPhoto = person.photos[0].url
+		console.log(srcOfPhoto)
+
+		$('#photo').attr("src", srcOfPhoto);
+		$('#gender').text('Gender: '+person.demographics.gender);
+		$('#location').text('Location: '+person.demographics.locationGeneral)
+
+
+		for (var i = 0; i < person.organizations.length; i++) {
+			if (person.organizations[i].current === true) {
+				var currentRoleTitle = person.organizations[i].title;
+				var currentRoleCompany = person.organizations[i].name;
+			}
+		}
+
+		$('#currentJob').text('Current Role: '+currentRoleTitle+" at "+currentRoleCompany)
+		$('#website').text('Website: '+person.contactInfo.websites[0].url)
+	
+	}
+	
+		else {
+			console.log('Error!')
+		}
+
+	});
+	});
+
+	$('#submitButtonTwitter').click(function() {
+		var twitterHandle = $('#twitterHandle').val();
+	
+
+	$.fullcontact.twitterLookup('fdd4a159a9943b57', twitterHandle, function(obj) {
+		console.log(obj);
+		
+		if (obj.status == 200) {
+
+
+
+		var person = obj
+
+		console.log("Identity found!")
+		console.log(person)
+
+		$('#fullNameHeader').text('Found '+person.contactInfo.fullName+'!')
+		console.log(person.contactInfo.fullName)
+
+		var srcOfPhoto = person.photos[0].url
+		console.log(srcOfPhoto)
+
+		$('#photo').attr("src", srcOfPhoto);
+		$('#gender').text('Gender: '+person.demographics.gender);
+		$('#location').text('Location: '+person.demographics.locationGeneral)
+
+
+		for (var i = 0; i < person.organizations.length; i++) {
+			if (person.organizations[i].current === true) {
+				var currentRoleTitle = person.organizations[i].title;
+				var currentRoleCompany = person.organizations[i].name;
+			}
+		}
+
+		$('#currentJob').text('Current Role: '+currentRoleTitle+" at "+currentRoleCompany)
+		$('#website').text('Website: '+person.contactInfo.websites[0].url)
+	
+	}
+	
+		else {
+			console.log('Error!')
+		}
+	});
+	});
+
+
+});
