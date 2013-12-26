962c0a2e19550096292ef947f1459641a34d1724
alexschiff <alex.schiff1128@gmail.com>
2013-12-26 01:25:43 -0500

    Added social buttons, fixed duplicate issue for summary info

--- scripts.js
+++ scripts.js
@@ -9,87 +9,112 @@ $(document).ready(function() {
 		
 		if (obj.status == 200) {
 
+			var person = obj
 
+			console.log("Identity found!")
+			console.log(person)
 
-		var person = obj
-
-		console.log("Identity found!")
-		console.log(person)
-
-		$('#fullNameHeader').text('Found '+person.contactInfo.fullName+' with '+person.likelihood+' likelihood!')
-		console.log(person.contactInfo.fullName)
-
-		var srcOfPhoto = person.photos[0].url
-		console.log(srcOfPhoto)
-
-		$('#photo').attr("src", srcOfPhoto);
-		$('#gender').text('Gender: '+person.demographics.gender);
-		$('#location').text('Location: '+person.demographics.locationGeneral)
-		$('#website').text('Website: '+person.contactInfo.websites[0].url)
-
-		for (var i = 0; i < person.organizations.length; i++) {
-			if (person.organizations[i].current === true) {
-				var currentRoleTitle = person.organizations[i].title;
-				var currentRoleCompany = person.organizations[i].name;
+			if (person.hasOwnProperty('contactInfo') === false) {
+				alert("Email exists, but no information found.")
 			}
-		}		
 
-		$('#currentJob').text('Current Role: '+currentRoleTitle+" at "+currentRoleCompany)
+			$('#fullNameHeader').text('Found '+person.contactInfo.fullName+' with '+person.likelihood+' likelihood!')
+			console.log(person.contactInfo.fullName)
 
-	
-	}
+			var srcOfPhoto = person.photos[0].url
+			console.log(srcOfPhoto)
+
+			$('#photo').attr("src", srcOfPhoto);
+
+			
+			if (person.hasOwnProperty('demographics') === true) {
+
+				if (person.demographics.hasOwnProperty('gender') === true) {
+
+				$('#gender').text('Gender: '+person.demographics.gender);
+
+				}
+			}
+
+			else {
+				$('#gender').text('Gender: Undefined');
+			}
+
+			
+			if (person.hasOwnProperty('demographics') === true) {
+				
+				if (person.demographics.hasOwnProperty('locationGeneral') === true) {
+
+				$('#location').text('Location: '+person.demographics.locationGeneral);
+
+				}
+			}
+
+			else {
+				$('#location').text('Location: Undefined');
+			}
+
+			
+			if (person.contactInfo.hasOwnProperty('websites') === true) {
+				$('#website').text('Website: '+person.contactInfo.websites[0].url);
+			}
+
+			else {
+				$('#website').text('Website: Undefined');
+			}
+
+			
+			if (person.hasOwnProperty('organizations') === true) {
+			
+				for (var i = 0; i < person.organizations.length; i++) {
+					if (person.organizations[i].current === true) {
+						var currentRoleTitle = person.organizations[i].title;
+						var currentRoleCompany = person.organizations[i].name;
+					}
+				}
+
+				$('#currentJob').text('Current Role: '+currentRoleTitle+" at "+currentRoleCompany)
+			}
+
+			else {
+				$('#currentJob').text('Current Role: Undefined')
+			}
+
+			if (person.hasOwnProperty('socialProfiles') === true) {
+			
+				for (var i = 0; i < person.socialProfiles.length; i++) {
+					if (person.socialProfiles[i].typeName === "Twitter") {
+						var Twitter = person.socialProfiles[i].url;
+						$('#summaryContainer').append('<a href="'+Twitter+'"><img class="socialButton" id="Twitter" src="http://www.vjbooks.com/v/vspfiles/assets/images/official%20twitter%20button%20-%20300.png"/></a>')
+
+					}
+					if (person.socialProfiles[i].typeName === "Facebook") {
+						var Facebook = person.socialProfiles[i].url;
+						$('#summaryContainer').append('<a href="'+Facebook+'"><img class="socialButton" id="Facebook" src="http://web.mit.edu/misti/images/Facebook%20Button.png"/></a>')
+					}
+					if (person.socialProfiles[i].typeName === "LinkedIn") {
+						var LinkedIn = person.socialProfiles[i].url;
+						$('#summaryContainer').append('<a href="'+LinkedIn+'"><img class="socialButton" id="LinkedIn" src="http://www.ldcgasforums.com/Assets/Image/Linkedin_Button(1).jpg"/></a>')
+					}
+
+				}
+			}	
+
+		}
+
+
+
+
 	
 		else {
 			console.log('Error!')
 		}
 
 	});
-	});
-
-	$('#submitButtonTwitter').click(function() {
-		var twitterHandle = $('#twitterHandle').val();
 	
-
-	$.fullcontact.twitterLookup('fdd4a159a9943b57', twitterHandle, function(obj) {
-		console.log(obj);
-		
-		if (obj.status == 200) {
-
-
-
-		var person = obj
-
-		console.log("Identity found!")
-		console.log(person)
-
-		$('#fullNameHeader').text('Found '+person.contactInfo.fullName+'!')
-		console.log(person.contactInfo.fullName)
-
-		var srcOfPhoto = person.photos[0].url
-		console.log(srcOfPhoto)
-
-		$('#photo').attr("src", srcOfPhoto);
-		$('#gender').text('Gender: '+person.demographics.gender);
-		$('#location').text('Location: '+person.demographics.locationGeneral)
-
-
-		for (var i = 0; i < person.organizations.length; i++) {
-			if (person.organizations[i].current === true) {
-				var currentRoleTitle = person.organizations[i].title;
-				var currentRoleCompany = person.organizations[i].name;
-			}
-		}
-
-		$('#currentJob').text('Current Role: '+currentRoleTitle+" at "+currentRoleCompany)
-		$('#website').text('Website: '+person.contactInfo.websites[0].url)
-	
-	}
-	
-		else {
-			console.log('Error!')
-		}
 	});
-	});
-
 
 });
+
+
+// Need to do error-handling for when a property doesn't exist.
