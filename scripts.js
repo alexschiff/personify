46a5d617d061c30ac740a8188f3fdd5d576f1edd
alexschiff <alex.schiff1128@gmail.com>
2014-04-01 22:52:04 -0400

    Clean up clean up everybody everywhere

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 		var lastName = $('#lastName').val();
 		var domain = $('#domain').val();
 
-		var attemptsArray = [firstName+'@'+domain, firstName+'.'+lastName+'@'+domain, firstName[0]+'.'+lastName+'@'+domain, firstName[0]+lastName+'@'+domain, firstName+'_'+lastName+'@'+domain, firstName[0]+'_'+lastName+'@'+domain]
+		var attemptsArray = [firstName+'@'+domain, firstName+'.'+lastName+'@'+domain, firstName[0]+'.'+lastName+'@'+domain, firstName[0]+lastName+'@'+domain, firstName+'_'+lastName+'@'+domain, firstName[0]+'_'+lastName+'@'+domain, lastName+'@'+domain, firstName[0]+'@'+domain]
 
 		var correctAttempts = [];
 
@@ -14,7 +14,6 @@ $(document).ready(function() {
 		attemptsArray.forEach(function(attempt) {
 			$.fullcontact.emailLookup('fdd4a159a9943b57', attempt, function(obj) {
 				attemptsMade++;
-				console.log(attemptsMade);
 				
 				if (obj.status === 200) {
 					var Match = {
@@ -28,8 +27,6 @@ $(document).ready(function() {
 
 		if (attemptsMade === attemptsArray.length) {
 
-			console.log(correctAttempts);
-
 			var top = {
 				score: 0
 			};
@@ -39,7 +36,8 @@ $(document).ready(function() {
 					top = correctAttempts[i];
 				};
 			};
-			console.log("The most likely email is " + top.email);
+			console.log("The most likely email is " + top.email+'. However, click the triangle below to see what also returned results:');
+			console.log(correctAttempts);
 
 			$.fullcontact.emailLookup('fdd4a159a9943b57', top.email, function(obj) {
 				console.log(JSON.stringify(obj, null, 2));
@@ -122,7 +120,9 @@ $(document).ready(function() {
 						$('#currentJob').text('Current Role: Undefined')
 					}
 
-					$('#email').text('Email: '+top.email);
+					$('#email').text('Most likely email: '+top.email);
+
+					$('#console').text('Hacker? Open up the console to see more details.')
 
 					if (person.hasOwnProperty('socialProfiles') === true) {
 					
@@ -192,18 +192,14 @@ $(document).ready(function() {
 
 	});
 
-// So far I can get the most likely email, but it is stuck inside this function. I need to figure out how to make the local variable global. Or something like that? Fuck.
-
 	$('#submitButtonEmail').click(function() {
-		var email = $('#Email').val();
+		var email = $('#EmailSearch').val();
 	
 
 	$.fullcontact.emailLookup('fdd4a159a9943b57', email, function(obj) {
 		console.log(JSON.stringify(obj, null, 2));
 		
 		if (obj.status == 200) {
-
-			console.log("Identity found!")
 
 			var person = obj
 
@@ -239,7 +235,7 @@ $(document).ready(function() {
 				$('#gender').text('Gender: Undefined');
 			}
 
-			
+					
 			if (person.hasOwnProperty('demographics') === true) {
 				
 				if (person.demographics.hasOwnProperty('locationGeneral') === true) {
@@ -262,7 +258,11 @@ $(document).ready(function() {
 				$('#website').text('Website: Undefined');
 			}
 
-			
+			$('#email').text('Most likely email: '+$('#EmailSearch').val());
+
+			$('#console').text('Hacker? Open up the console to see more details.')
+
+
 			if (person.hasOwnProperty('organizations') === true) {
 			
 				for (var i = 0; i < person.organizations.length; i++) {
@@ -338,148 +338,6 @@ $(document).ready(function() {
 		if (obj.status === 202) {
 			alert('Never seen that email before! Try again in 5 minutes after we search the interwebs.')
 		}
-
-	});
-	
-	});
-
-	$('#submitButtonTwitter').click(function() {
-		var twitterHandle = $('#twitterHandle').val();
-	
-
-	$.fullcontact.twitterLookup('fdd4a159a9943b57', twitterHandle, function(obj) {
-		console.log(JSON.stringify(obj, null, 2));
-		
-		if (obj.status == 200) {
-
-			console.log("Identity found!")
-
-			var person = obj
-
-			if (person.hasOwnProperty('contactInfo') === false) {
-				alert("Twitter handle exists, but no information found.")
-			}
-
-			$('#fullNameHeader').text('Found '+person.contactInfo.fullName+' with '+person.likelihood+' likelihood!')
-
-			var srcOfPhoto = person.photos[0].url
-
-			$('#photo').attr("src", srcOfPhoto);
-
-			
-			if (person.hasOwnProperty('demographics') === true) {
-
-				if (person.demographics.hasOwnProperty('gender') === true) {
-
-				$('#gender').text('Gender: '+person.demographics.gender);
-
-				}
-			}
-
-			else {
-				$('#gender').text('Gender: Undefined');
-			}
-
-			
-			if (person.hasOwnProperty('demographics') === true) {
-				
-				if (person.demographics.hasOwnProperty('locationGeneral') === true) {
-
-				$('#location').text('Location: '+person.demographics.locationGeneral);
-
-				}
-			}
-
-			else {
-				$('#location').text('Location: Undefined');
-			}
-
-			
-			if (person.contactInfo.hasOwnProperty('websites') === true) {
-				$('#website').text('Website: '+person.contactInfo.websites[0].url);
-			}
-
-			else {
-				$('#website').text('Website: Undefined');
-			}
-
-			if (person.hasOwnProperty('digitalFootprint') === true) {
-				
-				if (person.digitalFootprint.hasOwnProperty('scores') === true) {
-
-				$('#Klout').text('Klout Score: '+person.digitalFootprint.scores[0].value);
-
-				}
-
-			else {
-				$('#Klout').text('Klout Score: Undefined')
-			}
-
-			}
-
-			
-			if (person.hasOwnProperty('organizations') === true) {
-			
-				for (var i = 0; i < person.organizations.length; i++) {
-					if (person.organizations[i].current === true) {
-						var currentRoleTitle = person.organizations[i].title;
-						var currentRoleCompany = person.organizations[i].name;
-					}
-				}
-
-				$('#currentJob').text('Current Role: '+currentRoleTitle+" at "+currentRoleCompany)
-			}
-
-			else {
-				$('#currentJob').text('Current Role: Undefined')
-			}
-
-
-			if (person.hasOwnProperty('socialProfiles') === true) {
-			
-
-				if (person.socialProfiles.hasOwnProperty('Twitter') === false) {
-					$('#Twitter').css('display', 'none')
-				}
-
-				if (person.socialProfiles.hasOwnProperty('Facebook') === false) {
-					$('#Facebook').css('display', 'none')
-				}
-
-				if (person.socialProfiles.hasOwnProperty('LinkedIn') === false) {
-					$('#LinkedIn').css('display', 'none')
-				}
-
-
-
-				for (var i = 0; i < person.socialProfiles.length; i++) {
-
-					if (person.socialProfiles[i].typeName === "Twitter") {
-						var Twitter = person.socialProfiles[i].url;
-						$('#Twitter').css('display', 'inline-block')
-						$('#Twitter').wrap('<a href="'+Twitter+'"></a>')
-					}
-					
-					if (person.socialProfiles[i].typeName === "Facebook") {
-						var Facebook = person.socialProfiles[i].url;
-						$('#Facebook').css('display', 'inline-block')
-						$('#Facebook').wrap('<a href="'+Facebook+'"></a>')
-					}
-
-					if (person.socialProfiles[i].typeName === "LinkedIn") {
-						var LinkedIn = person.socialProfiles[i].url;
-						$('#LinkedIn').css('display', 'inline-block')
-						$('#LinkedIn').wrap('<a href="'+LinkedIn+'"></a>')
-					}
-				}
-			}	
-
-		}
-
-		if (obj.status === 202) {
-			alert('Never seen that one before! Try again in 5 minutes after we search the interwebs.')
-		}
-
 
 	});
 	
